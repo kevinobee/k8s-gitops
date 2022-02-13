0cd1ccd9269686a114c07e658e33d4073a5f047e
Kevin Obee <kevin.obee@gmail.com>
2022-02-13 11:16:11 +0000

    disable analysis CLI tool installation

--- scripts/install-cli-tools.sh
+++ scripts/install-cli-tools.sh
@@ -10,22 +10,18 @@ set -o nounset;
 
 targetDir='/usr/local/bin/'
 
-# kubectl
-# ref: https://kubernetes.io/docs/tasks/tools/
-# curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-
 # kind - ref: https://kind.sigs.k8s.io/docs/user/quick-start/
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind ${targetDir}
 
-# Datree
-curl https://get.datree.io | /bin/bash
+# # Datree
+# curl https://get.datree.io | /bin/bash
 
-# Kubescape
-curl -s https://raw.githubusercontent.com/armosec/kubescape/master/install.sh | /bin/bash
+# # Kubescape
+# curl -s https://raw.githubusercontent.com/armosec/kubescape/master/install.sh | /bin/bash
 
-# kube-score
-curl -Lo ./kube-score https://github.com/zegl/kube-score/releases/download/v1.13.0/kube-score_1.13.0_linux_amd64
-chmod +x ./kube-score
-sudo mv ./kube-score ${targetDir}
+# # kube-score
+# curl -Lo ./kube-score https://github.com/zegl/kube-score/releases/download/v1.13.0/kube-score_1.13.0_linux_amd64
+# chmod +x ./kube-score
+# sudo mv ./kube-score ${targetDir}
