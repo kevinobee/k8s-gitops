f7bc9cb170c3a92ffd1e010afa288433a30eeefa
Kevin Obee <kevin.obee@gmail.com>
2022-02-13 11:20:44 +0000

    install krew CLI tool

--- scripts/install-cli-tools.sh
+++ scripts/install-cli-tools.sh
@@ -15,6 +15,26 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.11.1/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind ${targetDir}
 
+# krew - ref: https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+# macOS/Linux
+# Bash or ZSH shells
+# requires that git is installed
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+# add the $HOME/.krew/bin directory to your PATH environment variable
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+# ... TODO    and restart your shell.
+# check the installation
+kubectl krew
+
+
 # # Datree
 # curl https://get.datree.io | /bin/bash
 
