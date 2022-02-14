020142017b87aefc8e75a6de1aac81b94d18435e
Kevin Obee <kevin.obee@gmail.com>
2022-02-14 09:14:11 +0000

    leave cluster running after test execution

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# test.sh - create cluster, populate apps, test ingress and delete cluster
+# test.sh - create cluster, populate apps and run test on the cluster
 
 # standard bash error handling
 set -o errexit;
@@ -8,10 +8,6 @@ set -o nounset;
 # debug commands
 # set -x;
 
-# scripts/install-cli-tools.sh
 scripts/create-kind-cluster.sh
 scripts/install-apps.sh
 scripts/test-apps.sh
-scripts/delete-kind-cluster.sh
-
-
