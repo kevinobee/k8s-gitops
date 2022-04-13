9d1ae86dab18b013e68ec5222a435cb50521f58d
Kevin Obee <kevin.obee@gmail.com>
2022-04-13 16:24:16 +0100

    fix SC2155 (warning)
    Declare and assign separately to avoid masking return values

--- install.sh
+++ install.sh
@@ -58,7 +58,8 @@ if [[ ! $(kubectl get namespace | grep argocd) ]]; then
 fi
 
 kubectl -n argocd port-forward svc/argocd-server 8080:443 > /dev/null 2>&1 &
-export ARGOCD_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+ARGOCD_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
+export ARGOCD_PWD
 argocd login localhost:8080 --insecure --username admin --password "${ARGOCD_PWD}"
 
 echo
