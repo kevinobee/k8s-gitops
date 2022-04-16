b461246ebe4ec6a7321da63b48acedfe6d43bbbf
Kevin Obee <kevin.obee@gmail.com>
2022-04-16 17:21:26 +0700

    Manual linkerd viz install (#51)

--- install.sh
+++ install.sh
@@ -37,25 +37,31 @@ if [ ! $(kind get clusters --quiet) ]; then
   kubectl cluster-info --context kind-kind
 fi
 
-if [[ ! $(kubectl get namespace | grep linkerd) ]]; then
-  echo "Install Service Mesh ..."
-  kubectl kustomize apps/linkerd | kubectl apply -f -
-  linkerd check
-fi
+echo
+echo "Install Service Mesh ..."
+kubectl kustomize apps/linkerd | kubectl apply -f -
+linkerd check
 
-if [[ ! $(kubectl get namespace | grep argocd) ]]; then
-  echo "Install Argo CD ..."
-  kubectl kustomize apps/argocd | kubectl apply -f -
-  for deploy in "dex-server" "redis" "repo-server" "server"; \
-    do kubectl -n argocd rollout status deploy/argocd-${deploy}; \
-  done
-  kubectl -n argocd rollout status statefulset/argocd-application-controller
-fi
+echo
+echo "Install Argo CD ..."
+kubectl kustomize apps/argocd | kubectl apply -f -
+for deploy in "dex-server" "redis" "repo-server" "server"; \
+  do kubectl -n argocd rollout status deploy/argocd-${deploy}; \
+done
+kubectl -n argocd rollout status statefulset/argocd-application-controller
 
 kubectl -n argocd port-forward svc/argocd-server 8080:443 > /dev/null 2>&1 &
 export ARGOCD_PWD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --decode)
 argocd login localhost:8080 --insecure --username admin --password ${ARGOCD_PWD}
 
+echo
+echo
+echo "To open the Argo CD dashboard run:"
+echo
+echo "argocd admin dashboard -n argocd"
+echo
+echo "   user:     admin"
+echo "   password: ${ARGOCD_PWD}"
 echo
 echo
 echo "To create GitOps application in Argo CD (App of Apps) run:"
@@ -66,14 +72,11 @@ echo "argocd app wait gitops --health"
 echo "argocd app wait -l app.kubernetes.io/instance=gitops --sync"
 echo
 echo
-echo "To open the Argo CD dashboard run:"
-echo
-echo "argocd admin dashboard -n argocd"
-echo
-echo
 echo "To open the Linkerd UI run:"
 echo
-echo "linkerd viz dashboard"
+echo "linkerd viz install | kubectl apply -f -"
+echo "linkerd viz check"
+echo "linkerd viz dashboard &"
 echo
 echo
 echo "To install useful CLI tools run:"
